ff67a73ecf0fa8693ca8ecf382c43f97f353643f
SSKirillSS <SSTroveSS@Gmail.com>
2025-07-09 03:56:09 +0300

    Lil fix for Springy Boot model

--- src/main/java/it/hurts/sskirillss/relics/client/models/items/SpringyBootModel.java
+++ src/main/java/it/hurts/sskirillss/relics/client/models/items/SpringyBootModel.java
@@ -25,10 +25,10 @@ public class SpringyBootModel extends HumanoidModel<LivingEntity> {
         MeshDefinition meshDefinition = HumanoidModel.createMesh(new CubeDeformation(0.4F), 0F);
         PartDefinition partDefinition = meshDefinition.getRoot();
 
-        PartDefinition feetPart = partDefinition.addOrReplaceChild("feetPart", CubeListBuilder.create().texOffs(20, 5).addBox(-2.5F, 9.5F, -4.0F, 5.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 12).addBox(-2.5F, 6.5F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 1).addBox(-2.5F, 6.5F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(-0.25F))
-                .texOffs(20, 0).addBox(-2.5F, 9.5F, -4.0F, 5.0F, 3.0F, 2.0F, new CubeDeformation(-0.25F)), PartPose.offset(-2.0F, 11.5F, 0.0F));
+        PartDefinition feetPart = partDefinition.addOrReplaceChild("feetPart", CubeListBuilder.create().texOffs(20, 5).addBox(-2.5F, 9.5F, -4.5F, 5.0F, 3.0F, 2.0F, new CubeDeformation(0.25F))
+                .texOffs(0, 12).addBox(-2.5F, 6.5F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.25F))
+                .texOffs(0, 1).addBox(-2.5F, 6.5F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F))
+                .texOffs(20, 0).addBox(-2.5F, 9.5F, -4.5F, 5.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 11.5F, 0.0F));
 
         return LayerDefinition.create(meshDefinition, 64, 64);
     }
