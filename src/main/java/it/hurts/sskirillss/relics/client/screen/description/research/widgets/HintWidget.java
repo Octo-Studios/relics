b00a399dc71f485c6939bdea4d6df9c6041efb31
SSKirillSS <SSTroveSS@Gmail.com>
2025-07-23 06:14:14 +0300

    And another hint widget fix

--- src/main/java/it/hurts/sskirillss/relics/client/screen/description/research/widgets/HintWidget.java
+++ src/main/java/it/hurts/sskirillss/relics/client/screen/description/research/widgets/HintWidget.java
@@ -73,7 +73,7 @@ public class HintWidget extends AbstractDescriptionWidget implements IHoverableW
         if (relic.isAbilityResearched(minecraft.player, screen.stack, screen.ability)) {
             GUIRenderer.begin(DescriptionTextures.BULB_BROKEN, poseStack)
                     .anchor(SpriteAnchor.TOP_LEFT)
-                    .pos(getX() + 34, getY() - 3)
+                    .pos(getX() + 33, getY() - 3)
                     .end();
         } else {
             if (isHovered()) {
