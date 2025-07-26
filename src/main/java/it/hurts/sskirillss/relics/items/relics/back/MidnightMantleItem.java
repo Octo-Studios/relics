df5a758e4611f21f07819a8f3b7aebb98acf1b64
SSKirillSS <SSTroveSS@Gmail.com>
2025-07-26 11:42:55 +0300

    Added Midnight Mantle research patterns

--- src/main/java/it/hurts/sskirillss/relics/items/relics/back/MidnightMantleItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/back/MidnightMantleItem.java
@@ -14,6 +14,7 @@ import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.LevelingTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.loot.LootTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.loot.misc.LootEntries;
+import it.hurts.sskirillss.relics.items.relics.base.data.research.ResearchTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.style.StyleTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.style.TooltipData;
 import it.hurts.sskirillss.relics.network.NetworkHandler;
@@ -100,6 +101,10 @@ public class MidnightMantleItem extends RelicItem {
                                                 .formatValue((value) -> String.valueOf(MathUtils.round(value, 1)))
                                                 .build())
                                         .build())
+                                .research(ResearchTemplate.builder()
+                                        .star(0, 6, 10).star(1, 16, 10).star(2, 6, 21).star(3, 16, 21)
+                                        .link(0, 1).link(1, 3).link(3, 2).link(2, 1)
+                                        .build())
                                 .build())
                         .ability(AbilityTemplate.builder("invisibility")
                                 .rankModifier(3, "strike")
@@ -128,6 +133,9 @@ public class MidnightMantleItem extends RelicItem {
                                                 .formatValue((value) -> String.valueOf(MathUtils.round(value, 1)))
                                                 .build())
                                         .build())
+                                .research(ResearchTemplate.builder()
+                                        .star(0, 11, 6).star(1, 11, 10).star(2, 3, 16).star(3, 11, 16).star(4, 19, 16).star(5, 11, 21).star(6, 11, 25)
+                                        .link(3, 1).link(3, 2).link(3, 4).link(3, 5).link(1, 4).link(4, 5).link(5, 2).link(2, 1).link(1, 0).link(5, 6).build())
                                 .build())
                         .ability(AbilityTemplate.builder("constellation")
                                 .rankModifier(5, "stun")
@@ -167,6 +175,10 @@ public class MidnightMantleItem extends RelicItem {
                                         .upgradeModifier(ScalingModelRegistry.MULTIPLICATIVE_BASE.get(), 0.1D)
                                         .formatValue(value -> MathUtils.round(value, 1))
                                         .build())
+                                .research(ResearchTemplate.builder()
+                                        .star(0, 17, 4).star(1, 5, 9).star(2, 20, 15).star(3, 6, 20)
+                                        .link(0, 1).link(1, 2).link(2, 3)
+                                        .build())
                                 .build())
                         .ability(AbilityTemplate.builder("starfall")
                                 .rankModifier(7, "bounce")
@@ -195,6 +207,10 @@ public class MidnightMantleItem extends RelicItem {
                                         .initialValue(0.05D, 0.15D)
                                         .upgradeModifier(ScalingModelRegistry.MULTIPLICATIVE_BASE.get(), 0.1D)
                                         .formatValue(value -> (int) MathUtils.round(value * 100, 0))
+                                        .build())
+                                .research(ResearchTemplate.builder()
+                                        .star(0, 11, 4).star(1, 3, 11).star(2, 11, 11).star(3, 19, 11).star(4, 11, 18).star(5, 17, 21).star(6, 12, 23).star(7, 8, 26)
+                                        .link(2, 0).link(2, 1).link(2, 3).link(2, 4).link(0, 3).link(3, 4).link(4, 1).link(1, 0).link(4, 6).link(6, 5).link(6, 7)
                                         .build())
                                 .build())
                         .build())
