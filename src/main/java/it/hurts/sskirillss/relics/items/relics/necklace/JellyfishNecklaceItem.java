0ed3ed3dc11dd05650af6b8fc6aee5d995b10730
SSKirillSS <SSTroveSS@Gmail.com>
2025-07-30 00:24:15 +0300

    Added Jellyfish Necklace research entries

--- src/main/java/it/hurts/sskirillss/relics/items/relics/necklace/JellyfishNecklaceItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/necklace/JellyfishNecklaceItem.java
@@ -11,6 +11,7 @@ import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.LevelingTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.loot.LootTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.loot.misc.LootEntries;
+import it.hurts.sskirillss.relics.items.relics.base.data.research.ResearchTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.style.StyleTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.style.TooltipData;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
@@ -50,6 +51,10 @@ public class JellyfishNecklaceItem extends RelicItem {
                                         .initialValue(0.15D, 0.35D)
                                         .upgradeModifier(RelicsScalingModels.LOGARITHMIC.get(), 0.46D)
                                         .formatValue(value -> (int) MathUtils.round(value * 100, 0))
+                                        .build())
+                                .research(ResearchTemplate.builder()
+                                        .star(0, 11, 10).star(1, 3, 9).star(2, 19, 9).star(3, 2, 16).star(4, 11, 16).star(5, 20, 16).star(6, 11, 24)
+                                        .link(0, 2).link(2, 5).link(5, 6).link(6, 3).link(3, 1).link(1, 0).link(0, 4).link(4, 6).link(3, 4).link(4, 5)
                                         .build())
                                 .build())
                         .ability(AbilityTemplate.builder("shock")
@@ -108,6 +113,10 @@ public class JellyfishNecklaceItem extends RelicItem {
                                         .initialValue(2.5D, 5D)
                                         .upgradeModifier(RelicsScalingModels.RADICAL.get(), 4.226D)
                                         .formatValue(value -> MathUtils.round(value, 1))
+                                        .build())
+                                .research(ResearchTemplate.builder()
+                                        .star(0, 11, 4).star(1, 4, 11).star(2, 11, 11).star(3, 18, 11).star(4, 11, 18).star(5, 6, 21).star(6, 16, 21).star(7, 5, 24).star(8, 17, 24).star(9, 4, 27).star(10, 18, 27).star(11, 8, 29).star(12, 14, 29)
+                                        .link(4, 12).link(12, 11).link(11, 4).link(5, 6).link(7, 8).link(9, 10).link(0, 3).link(3, 4).link(4, 1).link(1, 0).link(0, 2).link(2, 4).link(1, 2).link(2, 3)
                                         .build())
                                 .build())
                         .build())
