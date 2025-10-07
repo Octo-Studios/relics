9b89e4d667833b3a96b676ed2b08c3a8589c5d4f
SSKirillSS <SSTroveSS@Gmail.com>
2025-10-07 15:23:56 +0300

    Some Piglin Mask progress

--- src/main/java/it/hurts/sskirillss/relics/items/relics/head/PiglinMaskItem.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/head/PiglinMaskItem.java
@@ -1,22 +1,26 @@
 package it.hurts.sskirillss.relics.items.relics.head;
 
-import it.hurts.sskirillss.relics.api.relics.AbilityMetricTemplate;
-import it.hurts.sskirillss.relics.api.relics.AbilityStatisticTemplate;
 import it.hurts.sskirillss.relics.api.relics.RelicTemplate;
-import it.hurts.sskirillss.relics.api.relics.VisibilityState;
 import it.hurts.sskirillss.relics.api.relics.abilities.AbilitiesTemplate;
 import it.hurts.sskirillss.relics.api.relics.abilities.AbilityTemplate;
-import it.hurts.sskirillss.relics.api.relics.abilities.ExperienceSourceTemplate;
-import it.hurts.sskirillss.relics.api.relics.abilities.ExperienceSourcesTemplate;
 import it.hurts.sskirillss.relics.api.relics.abilities.stats.StatTemplate;
+import it.hurts.sskirillss.relics.init.RelicsItems;
 import it.hurts.sskirillss.relics.init.RelicsScalingModels;
 import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.LevelingTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.loot.LootTemplate;
 import it.hurts.sskirillss.relics.items.relics.base.data.loot.misc.LootEntries;
-import it.hurts.sskirillss.relics.items.relics.base.data.research.ResearchTemplate;
+import it.hurts.sskirillss.relics.utils.EntityUtils;
 import it.hurts.sskirillss.relics.utils.MathUtils;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import top.theillusivec4.curios.api.SlotContext;
 
 public class PiglinMaskItem extends RelicItem {
@@ -25,66 +29,28 @@ public class PiglinMaskItem extends RelicItem {
         return RelicTemplate.builder()
                 .abilities(AbilitiesTemplate.builder()
                         .ability(AbilityTemplate.builder("neutrality")
+                                .rankModifier(1, "legion")
                                 .initialMaxLevel(0)
-                                .research(ResearchTemplate.builder()
-                                        .star(0, 11, 2).star(1, 5, 6).star(2, 17, 6).star(3, 2, 10).star(4, 20, 10).star(5, 11, 11).star(6, 2, 17).star(7, 7, 17).star(8, 15, 17).star(9, 20, 17).star(10, 2, 24).star(11, 20, 24).star(12, 11, 25)
-                                        .link(0, 1).link(0, 2).link(2, 5).link(1, 5).link(5, 7).link(5, 8).link(7, 12).link(8, 12).link(7, 3).link(7, 6).link(7, 10).link(8, 4).link(8, 9).link(8, 11)
+                                .build())
+                        .ability(AbilityTemplate.builder("looting")
+                                .modes("tooth", "buff")
+                                .rankModifier(3, "frenzy")
+                                .stat(StatTemplate.builder("chance")
+                                        .initialValue(0.1D, 0.15D)
+                                        .upgradeModifier(RelicsScalingModels.MULTIPLICATIVE_BASE.get(), 0.1212D)
+                                        .formatValue(value -> MathUtils.round(value, 1))
+                                        .build())
+                                .stat(StatTemplate.builder("amount")
+                                        .initialValue(1D, 3D)
+                                        .upgradeModifier(RelicsScalingModels.ADDITIVE.get(), 0.25D)
+                                        .formatValue(Double::intValue)
                                         .build())
                                 .build())
-                        .ability(AbilityTemplate.builder("gliding")
-                                .rankModifier(1, "momentum")
-                                .rankModifier(3, "strike")
-                                .rankModifier(5, "resistance")
-                                .modes("enabled", "disabled")
-                                .stat(StatTemplate.builder("efficiency")
-                                        .initialValue(0.25D, 0.35D)
-                                        .thresholdValue(0D, 1D)
-                                        .upgradeModifier(RelicsScalingModels.MULTIPLICATIVE_BASE.get(), 0.0531D)
-                                        .formatValue(value -> (int) MathUtils.round(value * 100, 0))
-                                        .build())
-                                .stat(StatTemplate.builder("damage")
-                                        .initialValue(0.1D, 0.25D)
-                                        .upgradeModifier(RelicsScalingModels.MULTIPLICATIVE_BASE.get(), 0.2571D)
-                                        .formatValue(value -> (int) MathUtils.round(value * 100, 0))
-                                        .build())
-                                .stat(StatTemplate.builder("resistance")
-                                        .initialValue(0.05D, 0.15D)
-                                        .thresholdValue(0D, 0.75D)
-                                        .upgradeModifier(RelicsScalingModels.MULTIPLICATIVE_BASE.get(), 0.1143D)
-                                        .formatValue(value -> MathUtils.round(value * 100, 1))
-                                        .build())
-                                .experienceSources(ExperienceSourcesTemplate.builder()
-                                        .source(ExperienceSourceTemplate.builder("gliding")
-                                                .modeVisibilityState("disabled", VisibilityState.HIDDEN)
-                                                .build())
-                                        .source(ExperienceSourceTemplate.builder("strike")
-                                                .modeVisibilityState("disabled", VisibilityState.HIDDEN)
-                                                .rankModifierVisibilityState("strike", VisibilityState.OBFUSCATED)
-                                                .build())
-                                        .source(ExperienceSourceTemplate.builder("resistance")
-                                                .modeVisibilityState("disabled", VisibilityState.HIDDEN)
-                                                .rankModifierVisibilityState("resistance", VisibilityState.OBFUSCATED)
-                                                .build())
-                                        .build())
-                                .statistic(AbilityStatisticTemplate.builder()
-                                        .metric(AbilityMetricTemplate.builder("duration")
-                                                .formatValue((value) -> MathUtils.formatTime(value.intValue()))
-                                                .modeVisibilityState("disabled", VisibilityState.HIDDEN)
-                                                .build())
-                                        .metric(AbilityMetricTemplate.builder("damage")
-                                                .formatValue((value) -> String.valueOf(MathUtils.round(value, 1)))
-                                                .rankModifierVisibilityState("strike", VisibilityState.OBFUSCATED)
-                                                .modeVisibilityState("disabled", VisibilityState.HIDDEN)
-                                                .build())
-                                        .metric(AbilityMetricTemplate.builder("resistance")
-                                                .formatValue((value) -> String.valueOf(MathUtils.round(value, 1)))
-                                                .rankModifierVisibilityState("resistance", VisibilityState.OBFUSCATED)
-                                                .modeVisibilityState("disabled", VisibilityState.HIDDEN)
-                                                .build())
-                                        .build())
-                                .research(ResearchTemplate.builder()
-                                        .star(0, 2, 9).star(1, 20, 9).star(2, 5, 13).star(3, 17, 13).star(4, 11, 14).star(5, 2, 18).star(6, 20, 18).star(7, 5, 19).star(8, 11, 19).star(9, 17, 19).star(10, 7, 26).star(11, 15, 26)
-                                        .link(8, 4).link(8, 2).link(8, 3).link(8, 10).link(8, 11).link(2, 7).link(2, 0).link(0, 5).link(3, 9).link(3, 1).link(1, 6)
+                        .ability(AbilityTemplate.builder("barter")
+                                .stat(StatTemplate.builder("amount")
+                                        .initialValue(1D, 3D)
+                                        .upgradeModifier(RelicsScalingModels.ADDITIVE.get(), 0.25D)
+                                        .formatValue(Double::intValue)
                                         .build())
                                 .build())
                         .build())
@@ -106,11 +72,89 @@ public class PiglinMaskItem extends RelicItem {
 
     @Override
     public boolean makesPiglinsNeutral(SlotContext slotContext, ItemStack stack) {
-        return this.isAbilityUnlocked(slotContext.entity(), stack, "neutrality");
+        return this.canPlayerUseAbility(slotContext.entity(), stack, "neutrality");
     }
 
-    //    @EventBusSubscriber
-//    public static class CommonEvents {
-//
-//    }
+    @EventBusSubscriber
+    public static class CommonEvents {
+        @SubscribeEvent
+        public static void onLivingDamage1(LivingDamageEvent.Post event) {
+            var target = event.getEntity();
+
+            if (!(event.getSource().getEntity() instanceof LivingEntity source) || target instanceof AbstractPiglin)
+                return;
+
+            var level = source.level();
+
+            for (var stack : EntityUtils.findEquippedCurios(source, RelicsItems.PIGLIN_MASK.get())) {
+                var relic = (PiglinMaskItem) stack.getItem();
+
+                if (!relic.isAbilityRankModifierUnlocked(source, stack, "neutrality", "legion"))
+                    continue;
+
+                for (var piglin : level.getEntitiesOfClass(AbstractPiglin.class, source.getBoundingBox().inflate(32))) {
+                    if (piglin.getTarget() != null || !piglin.hasLineOfSight(target))
+                        continue;
+
+                    piglin.getBrain().setMemory(MemoryModuleType.ANGRY_AT, target.getUUID());
+                }
+            }
+        }
+
+        @SubscribeEvent
+        public static void onLivingDamage2(LivingDamageEvent.Post event) {
+            var source = event.getSource().getEntity();
+            var target = event.getEntity();
+
+            if (source == null)
+                return;
+
+            var level = source.level();
+
+            for (var stack : EntityUtils.findEquippedCurios(target, RelicsItems.PIGLIN_MASK.get())) {
+                var relic = (PiglinMaskItem) stack.getItem();
+
+                if (!relic.isAbilityRankModifierUnlocked(target, stack, "neutrality", "legion"))
+                    continue;
+
+                for (var piglin : level.getEntitiesOfClass(AbstractPiglin.class, target.getBoundingBox().inflate(32))) {
+                    if (piglin.getTarget() != null || !piglin.hasLineOfSight(source))
+                        continue;
+
+                    piglin.getBrain().setMemory(MemoryModuleType.ANGRY_AT, source.getUUID());
+                }
+            }
+        }
+
+        @SubscribeEvent
+        public static void onLivingDeath(LivingDeathEvent event) {
+            var source = event.getSource().getEntity();
+
+            if (source == null)
+                return;
+
+            var entity = event.getEntity();
+
+            var level = source.level();
+            var random = level.getRandom();
+
+            for (var stack : EntityUtils.findEquippedCurios(source, RelicsItems.PIGLIN_MASK.get())) {
+                var relic = (PiglinMaskItem) stack.getItem();
+
+                if (!relic.canPlayerUseAbility(entity, stack, "looting"))
+                    continue;
+
+                var amount = MathUtils.multicast(random, relic.getStatValue(entity, stack, "looting", "chance"), (int) relic.getStatValue(entity, stack, "looting", "amount"));
+
+                for (int i = 0; i < amount; i++) {
+                    var drop = new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), RelicsItems.GOLDEN_TOOTH.get().getDefaultInstance());
+
+                    drop.setPickUpDelay(20);
+                    drop.setDeltaMovement(MathUtils.randomFloat(random) * 0.35F, 0.25F + random.nextFloat() * 0.25F, MathUtils.randomFloat(random) * 0.35F);
+
+                    level.addFreshEntity(drop);
+                }
+            }
+        }
+    }
 }
--- src/main/java/it/hurts/sskirillss/relics/mixin/PiglinAiMixin.java
+++ src/main/java/it/hurts/sskirillss/relics/mixin/PiglinAiMixin.java
@@ -1,12 +1,11 @@
 package it.hurts.sskirillss.relics.mixin;
 
-import it.hurts.sskirillss.relics.api.relics.IRelicItem;
 import it.hurts.sskirillss.relics.init.RelicsItems;
+import it.hurts.sskirillss.relics.items.relics.head.PiglinMaskItem;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.monster.piglin.Piglin;
 import net.minecraft.world.entity.monster.piglin.PiglinAi;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,36 +15,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Mixin(PiglinAi.class)
 public abstract class PiglinAiMixin {
-//    @Shadow
-//    private static List<ItemStack> getBarterResponseItems(Piglin piglin) {
-//        return new ArrayList<>();
-//    }
-//
-//    @Inject(method = "stopHoldingOffHandItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/piglin/PiglinAi;throwItems(Lnet/minecraft/world/entity/monster/piglin/Piglin;Ljava/util/List;)V", ordinal = 0), cancellable = true)
-//    private static void tweakBartering(Piglin piglin, boolean bool, CallbackInfo ci) {
-//        Optional<Player> optional = piglin.getBrain().getMemory(MemoryModuleType.NEAREST_VISIBLE_PLAYER);
-//
-//        if (optional.isEmpty())
-//            return;
-//
-//        Player player = optional.get();
-//
-//        ItemStack stack = EntityUtils.findEquippedCurio(player, RelicsItems.BASTION_RING.get());
-//
-//        if (stack.getItem() instanceof IRelicItem relic) {
-//            for (int i = 0; i < Math.round(relic.getStatValue(player, stack, "trade", "rolls")); i++) {
-//                if (piglin.getRandom().nextBoolean()) {
-//                    PiglinAi.throwItems(piglin, getBarterResponseItems(piglin));
-//
-//                    relic.spreadRelicExperience(player, stack, 3);
-//                }
-//            }
-//
-//            ci.cancel();
-//        }
-//    }
+    @Shadow
+    private static List<ItemStack> getBarterResponseItems(Piglin piglin) {
+        return new ArrayList<>();
+    }
+
+    @Inject(method = "stopHoldingOffHandItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/piglin/PiglinAi;throwItems(Lnet/minecraft/world/entity/monster/piglin/Piglin;Ljava/util/List;)V"), cancellable = true)
+    private static void tweakBartering(Piglin piglin, boolean bool, CallbackInfo ci) {
+        var optional = piglin.getBrain().getMemory(MemoryModuleType.NEAREST_VISIBLE_PLAYER);
+
+        if (optional.isEmpty())
+            return;
+
+        var player = optional.get();
+        var random = player.getRandom();
+
+        var shouldCancel = false;
+
+        for (var stack : EntityUtils.findEquippedCurios(player, RelicsItems.PIGLIN_MASK.get())) {
+            var relic = (PiglinMaskItem) stack.getItem();
+
+            if (!relic.canPlayerUseAbility(player, stack, "barter"))
+                continue;
+
+            shouldCancel = true;
+
+            for (int i = 0; i < random.nextInt((int) relic.getStatValue(player, stack, "barter", "amount") + 1) + 1; i++) {
+                PiglinAi.throwItems(piglin, PiglinAiMixin.getBarterResponseItems(piglin));
+            }
+        }
+
+        if (shouldCancel)
+            ci.cancel();
+    }
 }
