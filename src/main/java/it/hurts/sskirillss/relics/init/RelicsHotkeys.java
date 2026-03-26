8907820d992e629b17356f01fd8b24df05216338
SSKirillSS <SSTroveSS@Gmail.com>
2026-03-26 12:00:10 +0300

    Removed unused keybind

--- src/main/java/it/hurts/sskirillss/relics/init/RelicsHotkeys.java
+++ src/main/java/it/hurts/sskirillss/relics/init/RelicsHotkeys.java
@@ -10,16 +10,14 @@ import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT_ALT;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT_SHIFT;
 
-@EventBusSubscriber(modid = Relics.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = Relics.MODID, value = Dist.CLIENT)
 public class RelicsHotkeys {
     private static final String CATEGORY = "Relics";
 
-    public static final KeyMapping ACTIVE_ABILITIES_LIST = new KeyMapping("key.relics.active_abilities_list", GLFW_KEY_LEFT_ALT, CATEGORY);
     public static final KeyMapping RESEARCH_RELIC = new KeyMapping("key.relics.research_relic", GLFW_KEY_LEFT_SHIFT, CATEGORY);
 
     @SubscribeEvent
     public static void onKeybindingRegistry(RegisterKeyMappingsEvent event) {
-        event.register(ACTIVE_ABILITIES_LIST);
         event.register(RESEARCH_RELIC);
     }
 }
