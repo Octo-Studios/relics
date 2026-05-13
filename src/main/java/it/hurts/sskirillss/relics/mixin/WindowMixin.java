34748b0f6ee6245d0c4cbb1dd0876d52fffc2b1e
SSKirillSS <SSTroveSS@Gmail.com>
2026-05-13 13:49:43 +0300

    Fixed GUI scale on 4k+ monitors

--- src/main/java/it/hurts/sskirillss/relics/mixin/WindowMixin.java
+++ src/main/java/it/hurts/sskirillss/relics/mixin/WindowMixin.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.platform.Window;
 import it.hurts.sskirillss.relics.client.screen.base.IAutoScaledScreen;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,7 +19,7 @@ public class WindowMixin {
         if (!(MC.screen instanceof IAutoScaledScreen screen))
             return;
 
-        cir.setReturnValue((double) window.calculateScale(screen.getAutoScale(), MC.isEnforceUnicode()));
+        cir.setReturnValue((double) this.relics$getAutoScale(window, screen, MC.isEnforceUnicode()));
     }
 
     @Inject(method = "getGuiScaledHeight", at = @At("HEAD"), cancellable = true)
@@ -29,7 +30,7 @@ public class WindowMixin {
         if (!(MC.screen instanceof IAutoScaledScreen screen))
             return;
 
-        double scale = window.calculateScale(screen.getAutoScale(), MC.isEnforceUnicode());
+        double scale = this.relics$getAutoScale(window, screen, MC.isEnforceUnicode());
         int height = (int) (window.getHeight() / scale);
 
         cir.setReturnValue((window.getHeight() / scale > height ? height + 1 : height));
@@ -43,9 +44,19 @@ public class WindowMixin {
         if (!(MC.screen instanceof IAutoScaledScreen screen))
             return;
 
-        double scale = window.calculateScale(screen.getAutoScale(), MC.isEnforceUnicode());
+        double scale = this.relics$getAutoScale(window, screen, MC.isEnforceUnicode());
         int width = (int) (window.getWidth() / scale);
 
         cir.setReturnValue((window.getWidth() / scale > width ? width + 1 : width));
+    }
+
+    @Unique
+    private int relics$getAutoScale(Window window, IAutoScaledScreen screen, boolean enforceUnicode) {
+        var scale = window.calculateScale(screen.getAutoScale(), enforceUnicode);
+
+        if (scale > 1 && scale % 2 != 0)
+            scale--;
+
+        return scale;
     }
 }
