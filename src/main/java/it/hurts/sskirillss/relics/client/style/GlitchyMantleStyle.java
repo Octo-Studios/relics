2220929bdf0bde105ad072ab4d95ef9541bec656
SSKirillSS <SSTroveSS@Gmail.com>
2026-05-16 04:29:06 +0300

    More Glitchy Mantle progress

---
+++ src/main/java/it/hurts/sskirillss/relics/client/style/GlitchyMantleStyle.java
@@ -0,0 +1,36 @@
+package it.hurts.sskirillss.relics.client.style;
+
+import it.hurts.octostudios.octolib.util.OctoColor;
+import it.hurts.sskirillss.relics.client.style.base.RelicStyle;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GlitchyMantleStyle extends RelicStyle {
+    @Override
+    public OctoColor getTopTooltipBorderColor(LivingEntity entity, ItemStack stack) {
+        return new OctoColor(0xff480085);
+    }
+
+    @Override
+    public OctoColor getBottomTooltipBorderColor(LivingEntity entity, ItemStack stack) {
+        return new OctoColor(0xff2b0046);
+    }
+
+    @Override
+    public OctoColor getTopTooltipBackgroundColor(LivingEntity entity, ItemStack stack) {
+        return new OctoColor(0xf010082c);
+    }
+
+    @Override
+    public OctoColor getBottomTooltipBackgroundColor(LivingEntity entity, ItemStack stack) {
+        return new OctoColor(0xf0140718);
+    }
+
+    @Override
+    public List<OctoColor> getItemNameColors(LivingEntity entity, ItemStack stack) {
+        return Arrays.asList(new OctoColor(0xFF50ffff), new OctoColor(0xFFff41ff));
+    }
+}
