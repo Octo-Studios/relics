3e2412e6d6c2f17be597eb87e9b2cfba0728d681
SSKirillSS <SSTroveSS@Gmail.com>
2026-05-27 23:49:26 +0300

    More Shield of Retaliation progress

--- src/main/java/it/hurts/sskirillss/relics/client/style/ShieldOfRetaliationStyle.java
+++ src/main/java/it/hurts/sskirillss/relics/client/style/ShieldOfRetaliationStyle.java
@@ -11,26 +11,26 @@ import java.util.List;
 public class ShieldOfRetaliationStyle extends RelicStyle {
     @Override
     public OctoColor getTopTooltipBorderColor(LivingEntity entity, ItemStack stack) {
-        return new OctoColor(0xffd9d1b0);
+        return new OctoColor(0xffa20101);
     }
 
     @Override
     public OctoColor getBottomTooltipBorderColor(LivingEntity entity, ItemStack stack) {
-        return new OctoColor(0xff84512d);
+        return new OctoColor(0xff510004);
     }
 
     @Override
     public OctoColor getTopTooltipBackgroundColor(LivingEntity entity, ItemStack stack) {
-        return new OctoColor(0xf0151210);
+        return new OctoColor(0xf0380a0c);
     }
 
     @Override
     public OctoColor getBottomTooltipBackgroundColor(LivingEntity entity, ItemStack stack) {
-        return new OctoColor(0xf0130904);
+        return new OctoColor(0xf00f0f0e);
     }
 
     @Override
     public List<OctoColor> getItemNameColors(LivingEntity entity, ItemStack stack) {
-        return Arrays.asList(new OctoColor(0xFFf5deb3), new OctoColor(0xFFcf5d36));
+        return Arrays.asList(new OctoColor(0xFFe42500), new OctoColor(0xFFa20101));
     }
 }
